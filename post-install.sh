10643bf822faa3caedb9595f7a0b1176fdd2db65
AquaWolf <0xAquaWolf@protonmail.com>
2023-06-23 17:53:53 -0400

    updates

--- post-install.sh
+++ post-install.sh
@@ -30,17 +30,17 @@ DNF_CONF=/etc/dnf/dnf.conf
 echo "max_parallel_downloads=10" >> "$DNF_CONF"
 echo "fastestmirror=True" >> "$DNF_CONF"
 
-# update the system
+# # update the system
 
-sudo dnf update -y
-sudo dnf upgrade -y
+# sudo dnf update -y
+# sudo dnf upgrade -y
 
-# enable RPM Fussion
-sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+# # enable RPM Fussion
+# sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf upgrade --refresh
-sudo dnf groupupdate core
+# sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# sudo dnf upgrade --refresh
+# sudo dnf groupupdate core
 
 
 
