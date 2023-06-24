57fabda40b4d1315d212e0734b6713a5951113e1
AquaWolf <0xAquaWolf@protonmail.com>
2023-06-23 20:19:26 -0400

    up

--- post-install.sh
+++ post-install.sh
@@ -25,30 +25,33 @@ echo "Fedora Post Install Script"
 main() {
   print_header
 
-  # Update dnf package manager to best fastestmirror
-
-  echo "updating dnf conf"
+  # ===== | Update dnf package manager to best fastestmirror | ==========
+  #
+  
+  current_status "updating dnf conf"
 
   DNF_CONF=/etc/dnf/dnf.conf
-
   echo "max_parallel_downloads=10" >> "$DNF_CONF"
   echo "fastestmirror=True" >> "$DNF_CONF"
   echo "deltarpm=True" >> "$DNF_CONF"
   
-  # # update the system
-
-  # sudo dnf update -y
-  # sudo dnf upgrade -y
+  # ===== | update the system | ==========
+  current_status "updating packages"
+  # sudo dnf update -y && sudo dnf upgrade -y
   
-  # enable RPM Fussion
-  #
+  # ===== | enable RPM Fusion | ==========
+  
+  # current_status "enabling RPM Fusion"
   # sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
   # sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
   # sudo dnf upgrade --refresh
   # sudo dnf groupupdate core
 }
 
+current_status() {
+  echo -e "\n"
+  echo $1;
+}
+
+
 main
-
-
-
